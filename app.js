4d33c1fc2cdd5fa7335966b49bf6e655b601d5eb
sakshamgupta11 <sakshamgupta2308@gmail.com>
2026-05-10 15:28:21 +0530

    Fix Render config

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
